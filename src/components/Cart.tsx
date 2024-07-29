5b30016604ad0acf371770efda6cb8af1afbc07f
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-07-29 16:18:07 +0100

    button hover styles

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,7 +1,7 @@
 function Cart() {
   return (
     <div>
-      <button className="w-[16rem] h-[4.4rem] text-[1.4rem] flex items-center justify-center gap-2 border border-[#87635A] rounded-full">
+      <button className="w-[16rem] h-[4.4rem] text-[1.4rem] flex items-center justify-center gap-2 border border-[#87635A] rounded-full hover:text-[#C73B0F] hover:border-[#C73B0F] transition-all duration-200">
         <img
           src="../../public/assets/images/icon-add-to-cart.svg"
           alt="add to cart icon"
