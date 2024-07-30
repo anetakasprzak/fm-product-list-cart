e953d86f90b6390f23e3e3b1b039039f61691ac5
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-07-30 18:42:06 +0100

    create data and map

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import Cart from "./components/Cart";
 import Card from "./components/Card";
+import { fakeData } from "./data";
 
 function App() {
   return (
     <div>
       <h1 className="text-red-500">Desserts</h1>
       <div>
-        <Card />
+        {fakeData?.map((item) => (
+          <Card name={item.name} key={item.name} />
+        ))}
+
         <Cart />
       </div>
     </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,9 @@
-function Card() {
-  return <div className="">Card</div>;
+interface CardProps {
+  name: string;
+}
+
+function Card({ name }: CardProps) {
+  return <div className="">Card {name}</div>;
 }
 
 export default Card;
