d930476c8ef99277549487ab45fc3f729e4fb5c8
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-07-31 10:13:29 +0100

    responsive images jsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -17,6 +17,15 @@ function Card({ item }: CardProps) {
 
   return (
     <div>
+      <div className="w-[25rem]">
+        <picture>
+          <source srcSet={image.mobile} media="(max-width: 23.4em)" />
+          <source srcSet={image.tablet} media="(max-width: 47.8em)" />
+          <source srcSet={image.desktop} media="(max-width: 90em)" />
+          <img src={image.desktop} alt="image" />
+        </picture>
+      </div>
+
       <button className="w-[16rem] h-[4.4rem] text-[1.4rem] flex items-center justify-center gap-2 border border-[#87635A] rounded-full hover:text-[#C73B0F] hover:border-[#C73B0F] transition-all duration-200">
         <img
           src="../../public/assets/images/icon-add-to-cart.svg"
