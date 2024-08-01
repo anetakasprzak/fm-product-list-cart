164dccaf04befce905765b48a65686a5a98276f2
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-08-01 15:12:21 +0100

    heading styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import { fakeData } from "./data";
 function App() {
   return (
     <div>
-      <h1 className="text-red-500">Desserts</h1>
+      <h1 className="text-[#260F08] font-[700] text-[4rem] mb-[3.2rem]">
+        Desserts
+      </h1>
       <div>
         {fakeData?.map((item) => (
           <Card key={item.name} item={item} />
