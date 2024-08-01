b83eabc4abd139fa0df5a7f85a9bde96924d6731
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-08-01 15:56:21 +0100

    cart jsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,16 @@ import { fakeData } from "./data";
 
 function App() {
   return (
-    <div>
+    <div className="bg-[#FCF8F6]">
       <h1 className="text-[#260F08] font-[700] text-[4rem] mb-[3.2rem]">
         Desserts
       </h1>
-      <div>
-        {fakeData?.map((item) => (
-          <Card key={item.name} item={item} />
-        ))}
-
+      <div className="flex">
+        <div className="flex-auto">
+          {fakeData?.map((item) => (
+            <Card key={item.name} item={item} />
+          ))}
+        </div>
         <Cart />
       </div>
     </div>
--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,5 +1,14 @@
 function Cart() {
-  return <div>CART</div>;
+  return (
+    <div className="w-[38.4rem] h-max bg-white">
+      <h2>Your Cart (0)</h2>
+      <img
+        src="../../public/assets/images/illustration-empty-cart.svg"
+        alt="empty cart image"
+      />
+      <p>Your added items will appear here</p>
+    </div>
+  );
 }
 
 export default Cart;
