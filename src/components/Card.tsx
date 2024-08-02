a5150c992d51f03a8fdc96b97e9645f66a1a7e0c
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-08-02 16:13:04 +0100

    increase / decrase btns

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -16,7 +16,7 @@ interface CardProps {
 
 function Card({ item }: CardProps) {
   const { name, category, price, image } = item || {};
-  const [isClicked, setIsClicked] = useState(false);
+  const [isClicked, setIsClicked] = useState(true);
 
   return (
     <div>
@@ -43,8 +43,38 @@ function Card({ item }: CardProps) {
             </button>
           )}
           {isClicked && (
-            <div className="w-[16rem] h-[4.4rem] text-[1.4rem]">
-              <p>CLICKED</p>
+            <div className="w-[16rem] h-[4.4rem]  bg-[#C73B0F] rounded-full absolute top-[90%] left-[50%] translate-x-[-50%] flex items-center justify-center gap-12">
+              <button className="border border-white py-[1rem] px-[0.6rem] rounded-full">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="10"
+                  height="2"
+                  fill="none"
+                  viewBox="0 0 10 2"
+                >
+                  <path
+                    d="M0 .375h10v1.25H0V.375Z"
+                    className="fill-white hover:fill-[#C73B0F]"
+                  />
+                </svg>
+              </button>
+
+              <span className="text-white text-[1.4rem]">1</span>
+
+              <button className="border border-white py-[0.6rem] px-[0.6rem] rounded-full">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="10"
+                  height="10"
+                  fill="none"
+                  viewBox="0 0 10 10"
+                >
+                  <path
+                    fill="#fff"
+                    d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"
+                  />
+                </svg>
+              </button>
             </div>
           )}
         </div>
