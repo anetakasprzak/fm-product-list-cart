6ea114cc3e6d6839821d3fb6f0a11cde957d8e59
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-08-05 14:46:17 +0100

    fix spacing

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,16 @@ import { fakeData } from "./data";
 function App() {
   return (
     <div className="bg-[#FCF8F6] p-[88px]">
-      <h1 className="text-[#260F08] font-[700] text-[4rem] mb-[3.2rem]">
-        Desserts
-      </h1>
       <div className="flex">
-        <div className="flex gap-x-[24px] flex-wrap flex-auto">
-          {fakeData?.map((item) => (
-            <Card key={item.name} item={item} />
-          ))}
+        <div className="flex flex-col">
+          <h1 className="text-[#260F08] font-[700] text-[4rem] mb-[3.2rem]">
+            Desserts
+          </h1>
+          <div className="flex gap-x-[2.4rem] gap-y-[3.2rem] flex-wrap flex-auto">
+            {fakeData?.map((item) => (
+              <Card key={item.name} item={item} />
+            ))}
+          </div>
         </div>
         <Cart />
       </div>
