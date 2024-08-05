c7235ded25de606f37d0189689f06bef0c13a3a6
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-08-05 15:07:48 +0100

    cart item jsx

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -4,7 +4,8 @@ function Cart() {
       <h2 className="text-[#C73B0F] font-[700] text-[2.4rem] mb-[2.5rem]">
         Your Cart (0)
       </h2>
-      <div className="flex flex-col items-center py-3">
+      {/* EMPTY CART */}
+      {/* <div className="flex flex-col items-center py-3">
         <img
           src="../../public/assets/images/illustration-empty-cart.svg"
           alt="empty cart image"
@@ -13,7 +14,25 @@ function Cart() {
         <p className="text-[#87635A] font-[600] text-[1.4rem]">
           Your added items will appear here
         </p>
+      </div> */}
+
+      <CartItem />
+    </div>
+  );
+}
+
+function CartItem() {
+  return (
+    <div className="flex">
+      <div className="flex flex-col">
+        <h4>item name</h4>
+        <div>
+          <span>1x</span>
+          <span>@ £7.00</span>
+          <span>£28.00</span>
+        </div>
       </div>
+      <button>x</button>
     </div>
   );
 }
